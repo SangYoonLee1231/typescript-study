448fb04b6122a34f1d3415274e8cbb28c643117a
SangYoonLee <prestro1231@hufs.ac.kr>
2024-09-20 15:02:11 +0900

    Add: section1과 section2 업로드

---
+++ section2/src/chapter1.ts
@@ -0,0 +1,36 @@
+// number
+let num1: number = 123;
+let num2: number = -123;
+let num3: number = 0.123;
+let num4: number = -0.123;
+let num5: number = Infinity;
+let num6: number = -Infinity;
+let num7: number = NaN;
+// num1 = "hello";
+// num1.toUpperCase();
+num1.toFixed();
+
+// sting
+let str1: string = "hello";
+let str2: string = `hello ${num1}`;
+// str1.toFixed();
+str1.toUpperCase();
+
+// boolean
+let bool1: boolean = true;
+let bool2: boolean = false;
+
+// null
+let null1: null = null;
+
+// undefined
+let unde1: undefined = undefined;
+
+// strictNullChecks
+let numA: number = null;
+
+// 리터럴 타입
+let numB: 10 = 10;
+// numB = 12;
+let strA: "hello" = "hello";
+let boolA: true = true;
